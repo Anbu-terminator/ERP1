9b2c454e034585e0b10a01099b2a29ea502eb4a8
Anbu Sivam.B <anbulegend101@gmail.com>
2024-11-21 19:13:23 +0530

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const multer = require('multer');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
